d5fb333dc7530b8b20493f1a5bad4404dccdef52
zxy <2878091475@qq.com>
2022-09-26 15:28:30 +0800

    hot-fix commit2

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git222!!!");
         System.out.println("Hello Git3333333!!!");
         System.out.println("Hello hot-fix branch");
+        System.out.println("Hello hot-fix branch2222222222222222222");
     }
 }
